39796162d9e60b73a7501afaeb40fb8319efad1d
Aryan srivastava <srivastavaryan03@gmail.com>
2025-01-11 18:18:35 +0530

    server start setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,15 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+   {
+      languageOptions: {
+         globals: globals.browser,
+      },
+      rules: {
+         "no-unused-vars": "off",
+      },
+   },
+   pluginJs.configs.recommended,
 ];
--- server.mjs
+++ server.mjs
@@ -1,1 +1,9 @@
+import app from "./src/app.mjs";
 
+const startServer = () => {
+   const port = process.env.PORT || 8000;
+
+   app.listen(port, () => console.log(`Server Listening on port: ${port}`));
+};
+
+startServer();
